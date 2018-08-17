d566dc4a2803ac0b072057c8e39f1a5b9cfef4df
Chenqiqi0829 <625474141@qq.com>
2018-08-17 16:34:44 +0800

    second time

---
+++ examples/test_factorial.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+import unittest
+
+class test_factorial():
+
+    def factorial(self):
+        print("请输入要计算的阶乘数！")
+        num=input()
+        s=num
+        if num.isdigit():
+            for i in range(1,int(num)):
+                s=int(s)*int(i)
+            print("%s 的阶乘是：%s" %(num,s))
+        else:
+            print("请输入数字！")
+            m=test_factorial()
+            m.factorial()
+if __name__=='__main__':
+    a=test_factorial()
+    a.factorial()
